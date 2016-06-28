a0e7ce90567c44167adb4f01ba570a57b0d76533
DNADNL <dylan.levy@free.fr>
2016-06-28 10:09:30 -0400

    Translation : scriptFilesUpdate.sh

--- scriptFilesUpdate.sh
+++ scriptFilesUpdate.sh
@@ -1,11 +1,11 @@
-echo -en "Recherche : m68k-unknown-amigaos/Makefile "
+echo -en "Searching : m68k-unknown-amigaos/Makefile "
 
 while [ ! -f "toolchains/m68k-unknown-amigaos/Makefile" ]
 do
 	echo -en "."
 	sleep 0.5
 done
-echo " m68k-unknown-amigaos/Makefile trouvé !"
+echo " m68k-unknown-amigaos/Makefile found !"
 
 sed -f scriptUpdate/scriptUpdate_m68k-unknown-amigaos_Makefile.sed toolchains/m68k-unknown-amigaos/Makefile > toolchains/m68k-unknown-amigaos/MakefileNEW
 cp toolchains/m68k-unknown-amigaos/MakefileNEW toolchains/m68k-unknown-amigaos/Makefile
@@ -16,14 +16,14 @@ rm toolchains/m68k-unknown-amigaos/MakefileNEW
 
 
 
-echo -en "Recherche : sdk/Makefile "
+echo -en "Searching : sdk/Makefile "
 
 while [ ! -f "toolchains/sdk/Makefile" ]
 do
 	echo -en "."
 	sleep 0.5
 done
-echo " sdk/Makefile trouvé !"
+echo " sdk/Makefile found !"
 
 sed -f scriptUpdate/scriptUpdate_sdk_Makefile.sed toolchains/sdk/Makefile > toolchains/sdk/MakefileNEW
 cp toolchains/sdk/MakefileNEW toolchains/sdk/Makefile
@@ -34,14 +34,14 @@ rm toolchains/sdk/MakefileNEW
 
 
 
-echo -en "Recherche : c-parse.in "
+echo -en "Searching : c-parse.in "
 
 while [ ! -f "toolchains/m68k-unknown-amigaos/gcc-3.4.6/gcc/c-parse.in" ]
 do
 	echo -en "."
 	sleep 0.5
 done
-echo " c-parse.in trouvé !"
+echo " c-parse.in found !"
 
 sed -f scriptUpdate/scriptUpdate_c-parse.in.sed toolchains/m68k-unknown-amigaos/gcc-3.4.6/gcc/c-parse.in > toolchains/m68k-unknown-amigaos/gcc-3.4.6/gcc/c-parseNEW.in
 cp toolchains/m68k-unknown-amigaos/gcc-3.4.6/gcc/c-parseNEW.in toolchains/m68k-unknown-amigaos/gcc-3.4.6/gcc/c-parse.in
@@ -53,14 +53,14 @@ rm toolchains/m68k-unknown-amigaos/gcc-3.4.6/gcc/c-parseNEW.in
 
 
 
-echo -en "Recherche : collect2.c "
+echo -en "Searching : collect2.c "
 
 while [ ! -f "toolchains/m68k-unknown-amigaos/gcc-3.4.6/gcc/collect2.c" ]
 do
 	echo -en "."
 	sleep 0.5
 done
-echo " collect2.c trouvé !"
+echo " collect2.c found !"
 
 sed -f scriptUpdate/scriptUpdate_collect2.c.sed toolchains/m68k-unknown-amigaos/gcc-3.4.6/gcc/collect2.c > toolchains/m68k-unknown-amigaos/gcc-3.4.6/gcc/collect2NEW.c
 cp toolchains/m68k-unknown-amigaos/gcc-3.4.6/gcc/collect2NEW.c toolchains/m68k-unknown-amigaos/gcc-3.4.6/gcc/collect2.c
